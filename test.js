11a93ddfc0c1993de796143b05f43c4604940825
Farhan Halai <farhan.h@media.net>
2019-07-18 19:36:49 +0530

    path change

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 (function() {
   open(
     // "file:///C:/Users/farhan.h/Documents/Farhan/React%20Projects/final-nextjs-pwa/login.html",
-    "/login.html",
+    "/final-nextjs-pwa/login.html",
     "Test",
     "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=600,left=300,top=100"
   );
