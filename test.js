6f459fca9524248ce539ea316e46c485c7bbe9bb
Farhan Halai <farhan.h@media.net>
2019-07-19 20:35:44 +0530

    Cookies expire timing

--- test.js
+++ test.js
@@ -33,7 +33,19 @@
 
   function setCookie(cname, cvalue) {
     console.log("Set Cookie");
-    document.cookie = cname + "=" + cvalue + ";path=/";
+    var date = new Date();
+    date.setTime(date.getTime() + 24 * 60 * 60 * 1000);
+    // var offset = -d.getTimezoneOffset() / 60;
+    var expires = date.toGMTString();
+    document.cookie =
+      cname +
+      "=" +
+      cvalue +
+      "; expires=Session" +
+      // expires +
+      // "; timezone=" +
+      // offset +
+      "; path=/";
   }
 
   function eraseCookie(name) {
